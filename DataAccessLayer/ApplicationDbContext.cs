0657d3ea7440e986d10ddf2f4f3ef9bc7790a8e2
Eugene <eugeneweebdiz@gmail.com>
2021-03-21 16:45:29 +0300

    PublicDatas Add Migration

---
+++ DataAccessLayer/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using DataAccessLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccessLayer
+{
+    public class ApplicationDbContext: DbContext, IApplicationDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<PublicData> PublicDatas { get; set; }
+    }
+}
---
+++ DataAccessLayer/Migrations/20210321134249_InitialPublicDatasAdd.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccessLayer.Migrations
+{
+    public partial class InitialPublicDatasAdd : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PublicDatas",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Data = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PublicDatas", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PublicDatas");
+        }
+    }
+}
