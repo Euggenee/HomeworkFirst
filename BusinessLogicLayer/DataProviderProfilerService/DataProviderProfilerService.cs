48eda199abec992f3b1dd3089f887d6b2a3ab382
Eugene <eugeneweebdiz@gmail.com>
2021-04-06 14:28:25 +0300

    DataProviderProfilerService

--- BusinessLogicLayer/DataProviderProfilerService/DataProviderProfilerService.cs
+++ BusinessLogicLayer/DataProviderProfilerService/DataProviderProfilerService.cs
@@ -3,19 +3,22 @@ using System.Collections.Generic;
 using System.Text;
 using DataAccessLayer;
 using DataAccessLayer.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace BusinessLogicLayer.DataProviderProfilerService
 {
     public class DataProviderProfilerService : IDataProviderProfilerService
     {
         private readonly IApplicationDbContext _dbContext;
-        DataProviderProfilerService(IApplicationDbContext dbContext )
+     public DataProviderProfilerService(IApplicationDbContext dbContext )
         {
             _dbContext = dbContext;
         }
         public void ComparePerformance()
         {
-         
+            var employes = _dbContext.Employes.Include(e => e.HiringHistories);
+
+            
         }
     }
 }
---
+++ PresentationLayer/Controllers/DataProviderProfilerController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BusinessLogicLayer.DataProviderProfilerService;
+
+namespace PresentationLayer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DataProviderProfilerController : ControllerBase
+    {
+        private readonly IDataProviderProfilerService _dataProviderProfilerService;
+       public DataProviderProfilerController(IDataProviderProfilerService dataProviderProfilerService)
+        {
+            _dataProviderProfilerService = dataProviderProfilerService;
+        }
+
+        [HttpGet]
+        [Route("get-dataProviderProfiler")]
+        public ActionResult GetDataProviderProfiler()
+        {
+            _dataProviderProfilerService.ComparePerformance();
+            return Ok();
+        }
+    }
+}
